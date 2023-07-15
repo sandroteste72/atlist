614b8c1314eb1a92b37cfb042ec0396de2dabcbf
Sandro Arpi <sandro.teste72@gmail.com>
2023-07-15 00:27:37 -0300

    Project created

---
+++ src/main/java/com/arpitech/atlist/AtlistApplication.java
@@ -0,0 +1,13 @@
+package com.arpitech.atlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arpitech/atlist/AtlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpitech.atlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
